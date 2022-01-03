5a7cd2b9c895c8edce9eba3f7404ecc40091a2bf
Ennis Machta <esmachta@gmail.com>
2022-01-03 15:24:18 -0800

    testing firebase build

--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@ import "./index.css";
 import "./App.css";
 import reportWebVitals from "./reportWebVitals";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCiaCn7gTyqrBZykSfwhMfEAiJ_4LhKDl8",
+  authDomain: "tic-tac-toe-5f88b.firebaseapp.com",
+  projectId: "tic-tac-toe-5f88b",
+  storageBucket: "tic-tac-toe-5f88b.appspot.com",
+  messagingSenderId: "394540774988",
+  appId: "1:394540774988:web:f785890b51e810119d66e9",
+  measurementId: "G-TZECRNEGNZ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
